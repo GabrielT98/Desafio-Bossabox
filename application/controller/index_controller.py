9cbab9bca0db904e27fb01ae4840168d4be3d938
Gabriel <tamiozzo98@gmail.com>
2021-06-19 05:28:17 -0300

    Configurações básicas do html
    Colocando as tags básicas do html e criando o index.css

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,6 +1,10 @@
 from application import app
 from flask import Flask, render_template, redirect,url_for
 
+from application.model.dao.ferramenta_dao import FerramentaDAO
+
+ferramenta_dao = FerramentaDAO()
+list_ferramentas = ferramenta_dao.listar_ferramentas()
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",list_ferramentas = list_ferramentas)
